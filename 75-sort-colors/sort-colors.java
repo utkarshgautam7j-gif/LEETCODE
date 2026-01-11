cd6a4c39c49403c40fa50c3a0989d5696da291aa
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-01-11 20:06:37 +0530

    Time: 3 ms (13.72%) | Memory: 43.7 MB (13.76%) - LeetSync

---
+++ 75-sort-colors/sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        boolean isswapped;
+        int n= nums.length;
+        for(int i=0;i<n-1;i++){
+            isswapped=false;
+            for(int j=0;j<n-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                    isswapped=true;
+                }
+            }
+            if(isswapped== false) break;
+        }
+    }
+}
