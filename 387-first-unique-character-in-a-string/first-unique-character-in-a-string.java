8a3dee7a737e2f3fe27bc10043a2642e7b1aaffc
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-01-22 11:59:12 +0530

    Time: 5 ms (97.16%) | Memory: 47.5 MB (9.55%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,5 +1,25 @@
 class Solution {
     public int firstUniqChar(String s) {
+        int[] arr= new int[26];
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char ch= s.charAt(i);
+            arr[ch-'a']++;
+        }
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(arr[ch-'a']==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+/*
+class Solution {
+    public int firstUniqChar(String s) {
         int len=s.length();
         int ans=-1;
         l:
@@ -16,3 +36,5 @@ class Solution {
 
     }
 }
+
+*/
