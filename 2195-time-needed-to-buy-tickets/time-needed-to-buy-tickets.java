bc2d2680c053ed0dc48581ed2e3371f0f17a86d2
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-03-26 20:55:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (90.36%) - LeetSync

---
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int time =0;
+        for(int i=0;i<tickets.length;i++){
+            if(i<=k){
+                time =time+(Math.min(tickets[k],tickets[i]));
+            }
+            else{
+                time = time + (Math.min(tickets[k]-1,tickets[i]));
+            }
+        }
+        return time;
+    }
+}
