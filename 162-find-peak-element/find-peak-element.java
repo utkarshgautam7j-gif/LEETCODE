87a1511deeead0ba37881386d3d20a31b452fb65
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-03-27 23:19:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (48.93%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -14,3 +14,22 @@ class Solution {
         return ans;
     }
 }
+
+
+/*
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max = nums[0];
+        int ans=nums.length-1;
+        for(int i=1;i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+            }
+            else{
+                ans=i-1;
+            }
+        }
+        return ans;
+    }
+}
+*/
