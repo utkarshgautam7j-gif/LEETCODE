a084b0bc4bbbac980fba6157108152beddf6e6c9
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-03-29 00:22:36 +0530

    Time: 9 ms (14.97%) | Memory: 48.4 MB (18.55%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        l:
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[i].length;j++){
+                if(matrix[i][j]>target) continue l;
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
