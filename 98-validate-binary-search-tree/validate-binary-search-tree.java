6cc844429787fd398ad8b86f9e049da5684fe5d5
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-04-09 11:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (83.72%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     long prev = Long.MIN_VALUE;
 
@@ -10,5 +11,22 @@ class Solution {
         prev = root.val;
 
         return isValidBST(root.right);
+    }
+}
+*/
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean helper(TreeNode root, long min, long max) {
+        if (root == null) return true;
+
+        if (root.val <= min || root.val >= max) {
+            return false;
+        }
+
+        return helper(root.left, min, root.val) &&
+               helper(root.right, root.val, max);
     }
 }
