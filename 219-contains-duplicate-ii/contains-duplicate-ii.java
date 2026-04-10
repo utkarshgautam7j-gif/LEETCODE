d1dfb4072c87e751a6599225b1a6a99161bfc93b
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-04-10 23:53:32 +0530

    Time: 29 ms (15.21%) | Memory: 90.8 MB (27.81%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -1,5 +1,19 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                if(i-map.get(nums[i])<=k){
+                    return true;
+                }
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
 
-
+/*
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         //boolean ans=  false;
@@ -17,3 +31,5 @@ class Solution {
         return false;
     }
 }
+*/
+
