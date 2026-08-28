ec0ed143bb4066a6bc6e8a2d98e934176b233d3e
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-08-28 23:29:26 +0530

    Time: 2 ms (91.91%) | Memory: 47.7 MB (76.35%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -3,12 +3,11 @@ class Solution {
         int j=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[j++]=nums[i];
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
             }
-        }
-        while(j<nums.length){
-            nums[j]=0;
-            j++;
         }
     }
 }
