2ab7305bcdee0a98e68cc61a79b9ca5f4f180e06
UTKARSH GAUTAM <utkarshgautam7j@gmail.com>
2026-08-28 22:02:18 +0530

    Time: 1 ms (99.89%) | Memory: 55.9 MB (6.69%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,8 +1,20 @@
-import java.util.Arrays;
+//Boyer–Moore Majority Vote Algorithm
+
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-       Arrays.sort(nums);
-       return(nums[n/2]);
+        int count =0;
+        int candidate =0;
+        for(int x:nums){
+            if(count==0){
+                candidate=x;
+            }
+            if(candidate==x){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return candidate;
     }
 }
